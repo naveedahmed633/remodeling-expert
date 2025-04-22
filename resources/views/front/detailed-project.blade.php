9001267a7058a65fa4cf997c25dbe11ac226f487
naveexxs <naveexxs@gmail.com>
2025-04-22 22:30:38 +0500

    project-data

--- resources/views/front/detailed-project.blade.php
+++ resources/views/front/detailed-project.blade.php
@@ -8,15 +8,49 @@
             background-image: url('{{ $data?->getFirstMediaUrl('banner_image') ?: asset('front/images/image.png') }}');
             background-size: cover;
             background-position: center;
-            height: 400px;
-            /* Set a fixed height to avoid stretching */
+            height: 60vh; /* Responsive height */
+        }
+
+        /* Additional Styling for better UI */
+        .project-details {
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            background-color: #f9f9f9;
+            padding: 20px;
+        }
+
+        .project-details h5 {
+            font-weight: bold;
+        }
+
+        .project-images img {
+            max-width: 100%; /* Ensure image fits properly */
+            height: auto;
+        }
+
+        .project-title {
+            font-size: 2rem;
+            font-weight: bold;
+            color: #333;
+        }
+
+        .project-description {
+            font-size: 1.1rem;
+            line-height: 1.6;
+            color: #555;
+        }
+
+        /* Make sure all images inside col-md-6 or col-md-12 are responsive */
+        .project-images .col-md-6,
+        .project-images .col-md-12 {
+            padding: 10px;
         }
     </style>
 
     <!-- Banner Section -->
-    <div class="banner banner text-center">
+    <div class="banner text-center">
         <div class="mx-auto" style="max-width: 700px;">
-            <h1 data-aos="fade-up" class="text-white" style="font-weight: 700 !important;">{{ $project->title }}</h1>
+            <h1 data-aos="fade-up" class="text-white">{{ $project->title }}</h1>
         </div>
     </div>
 
@@ -28,14 +62,14 @@
                 <div class="col-md-8 mb-4">
                     <img data-aos="fade-up" src="{{ asset('front/images/image (18).png') }}" alt="Main Project Image"
                         class="img-fluid mb-3">
-                    <p data-aos="fade-up">{!! $project->description1 ?? '' !!}</p>
+                    <p data-aos="fade-up" class="project-description">{!! $project->description1 ?? '' !!}</p>
                 </div>
 
                 <!-- Right Column -->
                 <div class="col-md-4 mb-4">
-                    <div class="p-3 border rounded bg-light">
+                    <div class="project-details">
                         <h5 data-aos="fade-up" class="fw-bold mb-3">{{ $project->title }}</h5>
-                        <p data-aos="fade-up">{!! $project->description ?? '' !!}</p><br>
+                        <p data-aos="fade-up" class="project-description">{!! $project->description ?? '' !!}</p><br>
                         <div class="mb-2 d-flex">
                             <strong data-aos="fade-up" style="min-width: 100px;">Client</strong>
                             <span data-aos="fade-up">{{ $project->client }}</span>
@@ -54,7 +88,7 @@
                 <!-- Additional Images Section -->
                 <div class="col-md-8 mt-4">
                     <h4 data-aos="fade-up" class="mb-4">More About This Project</h4>
-                    <div class="row">
+                    <div class="row project-images">
                         @if ($project->image1)
                             <div class="col-md-6">
                                 <img data-aos="fade-up" src="{{ asset('storage/' . $project->image1) }}"
@@ -81,6 +115,4 @@
             </div>
         </div>
     </section>
-    {{-- 
-    <x-get-started :content="$content"/> --}}
 @endsection
